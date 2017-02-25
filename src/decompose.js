5233ca9d9317de20bf6499100fbc4b32ab33b0e2
Jonathan Delgado <hi@jon.soy>
2017-02-25 00:15:12 -0300

    Added src.

---
+++ src/decompose.js
@@ -0,0 +1,38 @@
+function isObject (objArg) { return Object(objArg) === objArg }
+
+function getKeys (objArg) {
+  return isObject(objArg) ? [].concat(Object.getOwnPropertySymbols(objArg), Object.getOwnPropertyNames(objArg)) : []
+}
+
+function decompose (objArg, prefix = [], history = new Set()) {
+  let collection = []
+
+  if (prefix.length === 0) {
+    collection.push([ [], objArg ])
+  }
+
+  getKeys(objArg).forEach((index) => {
+    const content = objArg[index]
+    const _i = [].concat(prefix, index)
+
+    if (!history.has(content)) {
+      if (isObject(content)) history.add(content)
+
+      collection.push([_i, content])
+
+      if (typeof (content) === 'object') {
+        const _o = decompose(content, _i, history)
+
+        collection = collection.concat(_o)
+      }
+    }
+  })
+
+  return collection
+}
+
+exports = module.exports = {
+  __esModule: true,
+  default: decompose,
+  decompose
+}
