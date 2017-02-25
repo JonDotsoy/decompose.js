8afc1bc342894cffacd7ecd8a546de04f23915b3
Jonathan Delgado <hi@jon.soy>
2017-02-25 02:49:31 -0300

    Update code apply order

--- src/eql.js
+++ src/eql.js
@@ -5,7 +5,7 @@ const {decompose} = require('./decompose')
 const listToSymbols = new Map()
 
 function normalizePath (pathArr) {
-  return pathArr.map(e=> {
+  return pathArr.map(e => {
     if (isSymbol(e)) {
       if (!listToSymbols.has(e)) listToSymbols.set(e, `@@SYMBOL(${uniqueId()})`)
       return listToSymbols.get(e)
@@ -16,20 +16,18 @@ function normalizePath (pathArr) {
 }
 
 function eql (deObjArg, deCompareObjArg) {
-
   if (deObjArg.length !== deCompareObjArg.length) return false
 
-  return [true].concat(deObjArg).reduce((e,[path, value]) => {
-    if (e===false) return false
+  return [true].concat(deObjArg).reduce((e, [path, value]) => {
+    if (e === false) return false
     const normPath = normalizePath(path)
 
-    const [,compareValue] = deCompareObjArg.find(([proposalComporePath, proposalComporeValue]) => {
-      return normPath === normalizePath( proposalComporePath )
+    const [, compareValue] = deCompareObjArg.find(([proposalComporePath, proposalComporeValue]) => {
+      return normPath === normalizePath(proposalComporePath)
     })
 
     return compareValue === value
   })
-
 }
 
 exports = module.exports = {
